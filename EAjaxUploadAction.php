e8b0493c2335c70e764695f03c8513593b02ed6d
Konstantinos Filios <kf@out-there-media.com>
2013-04-19 14:29:23 +0300

    Refactored qqFileUploader to remove optional validations from constructor

--- EAjaxUploadAction.php
+++ EAjaxUploadAction.php
@@ -14,16 +14,21 @@ class EAjaxUploadAction extends CAction
 	public function run()
 	{
 		Yii::import("ext.EAjaxUpload.qqFileUploader");
+
 		// list of valid extensions, ex. array("jpeg", "xml", "bmp")
 		$allowedExtensions = array("jpg");
 		// max file size in bytes
 		$sizeLimit = 1 * 1024 * 1024;
 
-		$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+		$uploader = new qqFileUploader();
+
+		$uploader->setAllowedExtensions($allowedExtensions);
+		$uploader->setSizeLimit($sizeLimit);
+
 		$result = $uploader->handleUpload('upload/');
+
 		// to pass data through iframe you will need to encode all html tags
-		$result = htmlspecialchars(json_encode($result), ENT_NOQUOTES);
-		echo $result;
+		echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
 	}
 
 }
