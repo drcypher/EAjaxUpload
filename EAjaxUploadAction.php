b9f4c060b0a1a8929bdd0fed107127cbacf291c6
Konstantinos Filios <kf@out-there-media.com>
2013-04-19 14:15:49 +0300

    Added phpdoc comments

--- EAjaxUploadAction.php
+++ EAjaxUploadAction.php
@@ -1,19 +1,29 @@
 <?php
-
+/**
+ * Sample fine uploader controller action.
+ *
+ * Handles the server-side functionality of fine uploader.
+ *
+ * @author Filios Konstantinos <konfilios@gmail.com>
+ * @author Vladimir Papaev <kosenka@gmail.com>
+ * @version 0.1
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
 class EAjaxUploadAction extends CAction
 {
+	public function run()
+	{
+		Yii::import("ext.EAjaxUpload.qqFileUploader");
+		// list of valid extensions, ex. array("jpeg", "xml", "bmp")
+		$allowedExtensions = array("jpg");
+		// max file size in bytes
+		$sizeLimit = 1 * 1024 * 1024;
 
-        public function run()
-        {
-                Yii::import("ext.EAjaxUpload.qqFileUploader");
+		$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+		$result = $uploader->handleUpload('upload/');
+		// to pass data through iframe you will need to encode all html tags
+		$result = htmlspecialchars(json_encode($result), ENT_NOQUOTES);
+		echo $result;
+	}
 
-                $allowedExtensions = array("jpg");
-                $sizeLimit = 1 * 1024 * 1024;
-
-                $uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
-                $result = $uploader->handleUpload('upload/eajaxupload/');
-                // to pass data through iframe you will need to encode all html tags
-                $result=htmlspecialchars(json_encode($result), ENT_NOQUOTES);
-                echo $result;
-        }
 }
